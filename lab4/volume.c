a792a310e17a57f0ac3895f4dbbf22a6d89fb799
icomanman <fred_barabs@yahoo.com>
2021-07-13 00:18:51 +0800

    lab4: solved volume.

--- lab4/volume.c
+++ lab4/volume.c
@@ -1,5 +1,3 @@
-// Modifies the volume of an audio file
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,6 +5,7 @@
 // Number of bytes in .wav header
 const int HEADER_SIZE = 44;
 
+// Modifies the volume of an audio file
 int main(int argc, char *argv[])
 {
     // Check command-line arguments
@@ -33,11 +32,30 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
-    // TODO: Copy header from input file to output file
+    uint8_t *bytes = malloc(sizeof(uint8_t));
+    // Copy header from input file to output file one at a time via a for loop:
+    for (int i = 0; i < HEADER_SIZE; i++)
+    {
+        fread(bytes, sizeof(uint8_t), 1, input);
+        fwrite(bytes, sizeof(uint8_t), 1, output);
+    }
 
-    // TODO: Read samples from input file and write updated data to output file
+    int16_t *buffer = malloc(sizeof(int16_t));
+    // Read samples from input file and write updated data to output file
+    while (fread(buffer, sizeof(int16_t), 1, input))
+    {
+        // scale the samples:
+        *buffer *= factor;
+        fwrite(buffer, sizeof(int16_t), 1, output);
+    }
 
     // Close files
     fclose(input);
     fclose(output);
+    // free the memory taken by malloc:
+    free(bytes);
+    free(buffer);
+    return 0;
 }
+
+// 2hrs: 12 Jul 2021

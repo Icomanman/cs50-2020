ef2df96e2641e98c1fb54a568bcc68d4db2b671f
icomanman <fred_barabs@yahoo.com>
2021-07-18 11:48:48 +0800

    starting with recover; setup debug config and make file.

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include <stdint.h>
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2)
+    {
+        printf("Usage: ./recover input.raw\n");
+        return 1;
+    }
+
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Unable to open file.\n");
+    }
+
+    uint8_t *buffer = malloc(sizeof(uint8_t));
+    int n = fread(buffer, sizeof(uint8_t), 1, input);
+    printf("The %i byte is %u\n", n, *buffer);
+
+    fclose(input);
+    free(buffer);
 }
+
+// 1+ hrs 18 July 2021

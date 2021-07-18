fb4fd9c1555e47c79ad6ce4d9064fa4724513b1e
icomanman <fred_barabs@yahoo.com>
2021-07-18 23:35:43 +0800

    writing files but no proper images yet.

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -21,12 +21,36 @@ int main(int argc, char *argv[])
         printf("Unable to open file.\n");
     }
 
-    BYTE *buffer = malloc(sizeof(BYTE));
-    int n = fread(buffer, sizeof(BYTE), 1, input);
-    printf("The 0e = %u and 0f = %u\n", e, f);
+    int n = 0;
+    int count = 0;
+    char filename[8]; // to hold the filename, say, 0001.jpg\0
+    BYTE *buffer = malloc(sizeof(BYTE) * 512);
+    do
+    {
+        // read 512 bytes at a time
+        n = fread(buffer, sizeof(BYTE), 512, input);
+        if (buffer[0] == f && buffer[1] == g && buffer[2] == f && (buffer[3] & 0xf0) == e)
+        {
+            // set the filenames
+            if (count < 10)
+            {
+                sprintf(filename, "00%i.jpg", count);
+            }
+            else if (count > 10 && count < 100)
+            {
+                sprintf(filename, "0%i.jpg", count);
+            }
 
+            FILE *out = fopen(filename, "w");
+            // printf("buffer[0] = %u, buffer[1] = %u, buffer[2] = %u, buffer[3] = %u\n", buffer[0], buffer[1], buffer[2], buffer[3]);
+            fwrite(buffer, sizeof(BYTE), 512, out);
+            fclose(out);
+            count += 1;
+        }
+    } while (n != 0);
+
+    // printf("Total Count: %i\n", count);
     fclose(input);
     free(buffer);
 }
-
-// 1.5+ hrs 18 July 2021
+// 3+ hrs 18 July 2021
